5e085acfdf319b8110f17e6b660b41be4cf2c0ba
Ramanish Singh <rsthakur922@gmail.com>
2020-09-06 12:11:57 -0500

    MD completed, documentation remaining

--- writer/cssi_cp2k/tests/simple-test.py
+++
@@ -1,9 +0,0 @@
-#quick test that works on my laptop
-from cssi_cp2k.classes import SIM as sim
-
-mySim = sim.SIM()
-mySim.GLOBAL.RUN_TYPE = "MC"
-mySim.write_changeLog()
-mySim.GLOBAL.RUN_TYPE = "ARb"
-mySim.write_errorLog()
-mySim.write_changeLog()
