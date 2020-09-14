8f96295fd589b371281240ab98e4815edd3bb109
Ramanish Singh <rsthakur922@gmail.com>
2020-09-14 08:14:01 -0500

    see if RTD  works

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,10 +27,19 @@ release = 'v0.0.1'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.todo', 'sphinx.ext.viewcode',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig', #'sphinx.ext.githubpages',
+    'sphinx.ext.doctest', 'sphinx.ext.intersphinx'
+]
+
+todo_include_todos = True
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
